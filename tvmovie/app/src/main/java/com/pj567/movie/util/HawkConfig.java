9450b4e0dcb21087f6c5678153b711149c1758dd
GitHub Action <action@github.com>
2021-04-28 03:55:26 +0000

    Action Update

---
+++ tvmovie/app/src/main/java/com/pj567/movie/util/HawkConfig.java
@@ -0,0 +1,25 @@
+package com.pj567.movie.util;
+
+/**
+ * @author pj567
+ * @date :2020/12/23
+ * @description:
+ */
+public class HawkConfig {
+    public static final String PASSWORD = "password";
+    public static final String FIRST = "first";
+    public static final String defaultPassword = "00000000";
+    public static final String ADOLESCENT_MODEL = "adolescent";
+    public static final String DEFAULT_SOURCE_ID = "default_source_id";
+    public static final String DEFAULT_PRASE_ID = "default_prase_id";
+    public static final String MEDIA_CODEC = "mediacodec";
+    public static final String INIT_X5 = "init_x5";
+    public static final String LIVE_CHANNEL = "live_channel";
+    public static final String PLAY_TYPE = "play_type";//0 系统；1 ijk；2 exo
+    public static final String LIVE_SOURCE = "live_source";//0 Ip多余域名；1 Ip少余域名
+    public static final String SHOW_UPDATE_HINT = "show_update_hint";//当前版本是否显示更新提示
+    public static final String APP_ID = "1111362713";
+    public static final String KAI_PING_ID = "4061164639676843";
+    public static final String JI_LI_ID = "6081064669178994";
+    public static final String CHA_PING_ID = "7081968720897289";
+}
